6a9fd5588a97c0435a0537289d932aba91f58c35
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-09-06 08:11:41 +0500

    m4

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 {
-    console.log("Masala-1")
-    function n(a){
+    // console.log("Masala-1")
+    // function n(a){
         
-        r=0
-        for(i=0 ; i<=a ; i+=2){
-            r+=i
-        }
-        return r
+    //     r=0
+    //     for(i=0 ; i<=a ; i+=2){
+    //         r+=i
+    //     }
+    //     return r
         
 
 
-    }
-    console.log(n(8))
+    // }
+    // console.log(n(8))
 }
 
 {
-    console.log("Masala-2")
-    function boolen(a){
-        if(a==true){
+    // console.log("Masala-2")
+    // function boolen(a){
+    //     if(a==true){
 
-            return 1
-        }else{
-            return 0
-        }
-    }
-    console.log(boolen(true))
+    //         return 1
+    //     }else{
+    //         return 0
+    //     }
+    // }
+    // console.log(boolen(true))
 }
 {
-    console.log("Masala-3")
-    function nm (a){
-        return a.slice(0,2)
-    }
-    console.log(nm("Ibrohim"))
+    // console.log("Masala-3")
+    // function nm (a){
+    //     return a.slice(0,2)
+    // }
+    // console.log(nm("Ibrohim"))
     
 }
 {
     console.log("Masala-4")
+    function son (a){
+        a= Math.trunc(a)+2
+        return a
+    }
+    console.log(son(6.25))
     
 }
