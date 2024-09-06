db60531cc8393194fe7a53983a1eb14f99ed9698
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-09-06 07:28:18 +0500

    masala

---
+++ app.js
@@ -0,0 +1,40 @@
+{
+    console.log("Masala-1")
+    function n(a){
+        
+        r=0
+        for(i=0 ; i<=a ; i+=2){
+            r+=i
+        }
+        return r
+        
+
+
+    }
+    console.log(n(8))
+}
+
+{
+    console.log("Masala-2")
+    function boolen(a){
+        if(a==true){
+
+            return 1
+        }else{
+            return 0
+        }
+    }
+    console.log(boolen(true))
+}
+{
+    console.log("Masala-3")
+    function nm (a){
+        return a.slice(0,2)
+    }
+    console.log(nm("Ibrohim"))
+    
+}
+{
+    console.log("Masala-4")
+    
+}
